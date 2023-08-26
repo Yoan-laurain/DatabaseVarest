99755a6ede6d1f79e4d7db9aea4dac2a9f5627ea
Yoan Laurain <yoan.laurain0@gmail.com>
2023-08-26 21:01:15 +0200

    Register call works

--- Source/Database/DatabaseGameModeBase.cpp
+++ Source/Database/DatabaseGameModeBase.cpp
@@ -1,5 +1,49 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
 #include "DatabaseGameModeBase.h"
+#include "OpenAPIAuthenticationApi.h"
+#include "OpenAPIAuthenticationApiOperations.h"
+#include "OpenAPIUserViewModel.h"
 
+bool ADatabaseGameModeBase::Authenticate()
+{
+	OpenAPI::OpenAPIUserViewModel UserViewModel;
+
+	UserViewModel.Email = "yoan.laurain1@gmail.com";
+	UserViewModel.Password = "Tatatutut33!";
+    
+	OpenAPI::OpenAPIAuthenticationApi::FRegisterUserPostDelegate RegisterUserDelegate;
+	RegisterUserDelegate.BindUObject(this, &ADatabaseGameModeBase::OnRegisterUserResponse);
+
+	OpenAPI::OpenAPIAuthenticationApi::RegisterUserPostRequest UserRequest;
+	UserRequest.OpenAPIUserViewModel = UserViewModel;
+
+	FHttpRequestPtr request = GetAuthenticationApi()->RegisterUserPost(UserRequest, RegisterUserDelegate);
+	
+	return true;
+}
+
+void ADatabaseGameModeBase::OnRegisterUserResponse(const OpenAPI::OpenAPIAuthenticationApi::RegisterUserPostResponse& Response)
+{
+	if (Response.IsSuccessful())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("User registered successfully!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("User registration failed: %s"), *Response.GetResponseString());
+	}
+}
+
+void ADatabaseGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	Authenticate();
+}
+
+OpenAPI::OpenAPIAuthenticationApi* ADatabaseGameModeBase::GetAuthenticationApi()
+{
+	if (AuthenticationApi == nullptr)
+	{
+		AuthenticationApi = new OpenAPI::OpenAPIAuthenticationApi();
+	}
+	return AuthenticationApi;
+}
--- Source/Database/DatabaseGameModeBase.h
+++ Source/Database/DatabaseGameModeBase.h
@@ -3,8 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "OpenAPIAuthenticationApi.h"
 #include "GameFramework/GameModeBase.h"
+#include "Interfaces/IHttpRequest.h"
 #include "DatabaseGameModeBase.generated.h"
+
+namespace OpenAPI
+{
+	class OpenAPIAuthenticationApi;
+}
 
 /**
  * 
@@ -13,5 +20,18 @@ UCLASS()
 class DATABASE_API ADatabaseGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public :
+
+		bool Authenticate();
+
+		OpenAPI::OpenAPIAuthenticationApi* GetAuthenticationApi();
 	
+		void OnRegisterUserResponse(const OpenAPI::OpenAPIAuthenticationApi::RegisterUserPostResponse& Response);
+	
+protected :
+	
+	OpenAPI::OpenAPIAuthenticationApi* AuthenticationApi;
+	
+	virtual void BeginPlay() override;
 };
